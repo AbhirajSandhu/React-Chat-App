7d5d175d6ce3ae4af508533121959e62a066a9de
AbhirajSandhu <abhriajsandhu0001@gmail.com>
2021-08-19 03:00:45 +0530

    basic structure built

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const router = require('./router');
+app.use(router);  // now we can pass it as a middleware
+
+io.on('connection', (socket) => {
+    console.log('New Connection Made!');
+
+    socket.console('disconnect', () => {
+        console.log('User had Left!')
+    })
+})
+// in a callback function we pass socket, this is going to be a socket
+// that will be connected as a client side socket
+// inside it only to disconnect, we will not pass any parameter as user had left
+
+const PORT = process.env.PORT || 5000
+server.listen( PORT, () => {
+    console.log('Listening on port: 3000');
+})
+
+/* simply
+    server.listen( 5000, () => {
+    console.log('Listening on port: 3000');
+}) */
